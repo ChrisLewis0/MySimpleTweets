ab2296823244579187c11c509768b9f4cca5078b
Christopher Lewis <cwlewis@fb.com>
2018-07-02 15:18:47 -0700

    Creates Tweet class

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,23 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Tweet {
+    // list out attributes
+    public String body;
+    public long uid; // database id for tweet
+    // public User user;
+    public String createAt;
+
+    // deserialize the JSON
+    public static Tweet fromJSON(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+
+        // extract the values from JSON
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("id");
+        tweet.createAt = jsonObject.getString("created_at");
+        return tweet;
+    }
+}
