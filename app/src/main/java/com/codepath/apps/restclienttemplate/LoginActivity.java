361d4188e430dabbb5caf9f127858f4c6d079da3
Christopher Lewis <cwlewis@fb.com>
2018-07-02 14:40:45 -0700

    Creates Timeline Activity
    Creates Timeline Activity with Recycler view and opens activity
    after login.

--- app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
@@ -1,10 +1,10 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.SampleModel;
 import com.codepath.apps.restclienttemplate.models.SampleModelDao;
@@ -45,9 +45,9 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	@Override
 	public void onLoginSuccess() {
 		// show that login was a success
-		Toast.makeText(this, "Succes", Toast.LENGTH_LONG).show();
-		// Intent i = new Intent(this, PhotosActivity.class);
-		// startActivity(i);
+		// Toast.makeText(this, "Succes", Toast.LENGTH_LONG).show();
+		Intent i = new Intent(this, TimelineActivity.class);
+		startActivity(i);
 	}
 
 	// OAuth authentication flow failed, handle the error
