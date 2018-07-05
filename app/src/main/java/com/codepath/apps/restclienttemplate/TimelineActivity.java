8284937ab7b7b1ed7a0071314c94115b3cac3b81
Christopher Lewis <cwlewis@fb.com>
2018-07-05 14:33:50 -0700

    Adds pull down refresh to Timeline
    When user pulls timeline down the timeline is refreshed including
    animation.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -2,6 +2,7 @@ package com.codepath.apps.restclienttemplate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -28,6 +29,8 @@ public class TimelineActivity extends AppCompatActivity {
     TweetAdapter tweetAdapter;
     ArrayList<Tweet> tweets;
     RecyclerView rvTweets;
+    private SwipeRefreshLayout swipeContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +38,31 @@ public class TimelineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline);
         // instantiates client
         client = TwitterApp.getRestClient(this);
+        // Lookup the swipe container view
+        swipeContainer = findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // refreshes timeline
+                populateTimeline();
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                swipeContainer.setRefreshing(false);
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
 
         // find RecyclerView
         rvTweets = findViewById(R.id.rvTweet);
         // init the arrayList (data source)
         tweets = new ArrayList<>();
-        // constract adapter from datasource
+        // construct adapter from data source
         tweetAdapter = new TweetAdapter(tweets);
         // set up Recycler View (layout manager, use adapter)
         rvTweets.setLayoutManager(new LinearLayoutManager(this));
@@ -59,7 +81,8 @@ public class TimelineActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-//                Log.d("TwitterClient", response.toString());
+                // clears previous data
+                tweetAdapter.clear();
                 // iterate through JSONArray
                 // for each entry, deserialize the JSON Object
                 for (int i = 0; i < response.length(); i++) {
