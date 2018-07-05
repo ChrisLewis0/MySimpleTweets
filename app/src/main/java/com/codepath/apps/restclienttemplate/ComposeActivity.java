ee7c63d381737c9228738c7011a2a3edba50076a
Christopher Lewis <cwlewis@fb.com>
2018-07-05 13:22:53 -0700

    Adds max word count on Tweets

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,10 +3,14 @@ package com.codepath.apps.restclienttemplate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -22,6 +26,8 @@ public class ComposeActivity extends AppCompatActivity {
     // constants
     TwitterClient client;
     EditText etTweet;
+    TextView tvWordCount;
+    int MAX_WORD_COUNT = 140;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +35,32 @@ public class ComposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compose);
         // instantiate client
         client = TwitterApp.getRestClient(this);
+        // find word count text
+        tvWordCount = findViewById(R.id.tvWordCount);
         // find tweet text
         etTweet = findViewById(R.id.etTweet);
+        // set tweet max character count
+        etTweet.setFilters(new InputFilter[] { new InputFilter.LengthFilter(MAX_WORD_COUNT) });
         // connect to submit button
+        etTweet.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String currentText = editable.toString();
+                int currentLength = currentText.length();
+                tvWordCount.setText(currentLength + " of 140 characters");
+            }
+        });
         final Button button = findViewById(R.id.bSubmit);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
