d7da9d4065fef7888313273c67b0e74e89c6086c
Christopher Lewis <cwlewis@fb.com>
2018-07-06 14:12:23 -0700

    Enables user to Retweet
    In tweet Timeline Details Activity, users are able to retweet
    by clicking on the retweet image button.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetDetailActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetDetailActivity.java
@@ -1,7 +1,10 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,11 +12,17 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
 import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
+import org.json.JSONObject;
 import org.parceler.Parcels;
+
+import cz.msebera.android.httpclient.Header;
 
 public class TweetDetailActivity extends AppCompatActivity {
 
+    // set up client
+    TwitterClient client;
     // the tweet to display
     Tweet tweet;
     // view objects
@@ -21,22 +30,46 @@ public class TweetDetailActivity extends AppCompatActivity {
     TextView tvUsername;
     TextView tvScreenName;
     TextView tvBody;
+    ImageButton ibRetweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // instantiates client
+        client = TwitterApp.getRestClient(this);
         setContentView(R.layout.activity_tweet_detail);
         // perform findViewById lookups
         ivProfileImage = findViewById(R.id.ivProfileImage);
         tvUsername = findViewById(R.id.tvUsername);
         tvScreenName = findViewById(R.id.tvScreenName);
         tvBody = findViewById(R.id.tvBody);
+        ibRetweet = findViewById(R.id.ibRetweet);
         // unwrap the tweet passed in via intent, using its simple name as a key
         tweet = Parcels.unwrap(getIntent().getParcelableExtra(Tweet.class.getSimpleName()));
         // populate views according to data
         tvUsername.setText(tweet.user.name);
         tvScreenName.setText("@" + tweet.user.screenName);
         tvBody.setText(tweet.body);
+        // enable retweet button
+        ibRetweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                client.reTweet(tweet.uid, new JsonHttpResponseHandler(){
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        // return to timeline
+                        Intent intent = new Intent(getApplicationContext(), TimelineActivity.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        super.onFailure(statusCode, headers, throwable, errorResponse);
+                    }
+                });
+            }
+        });
+
 
 
         Glide.with(this)
